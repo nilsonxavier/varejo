efc58f8bc6286f24bb566558ac6ae4597ccb5cac
nilsonxavier <nilsonxavier12@gmail.com>
2024-05-15 23:57:03 -0300

    adicionado methodo listar produtos

--- conexx/database.php
+++ conexx/database.php
@@ -28,6 +28,45 @@ class Database {
         // Fecha a declaração
         $stmt->close();
     }
+
+    // metodo lista dados 
+    public function listarDados($nomeDaTabela) {
+        // Query SQL para selecionar todos os dados da tabela
+        $sql = "SELECT * FROM $nomeDaTabela";
+        
+        // Prepara a declaração
+        $stmt = $this->conn->prepare($sql);
+        
+        // Executa a declaração
+        $stmt->execute();
+        
+        // Obtém os resultados
+        $result = $stmt->get_result();
+        
+        // Inicializa a string da tabela
+        $html = '<table class="table table-striped table-dark mt-2">';
+        $html .= '<tr><th>Nome</th><th>Quantidade</th><th>Ação</th></tr>';
+        
+        // Loop através dos resultados e adicionar cada linha à tabela HTML
+    while ($row = $result->fetch_assoc()) {
+        $html .= '<tr>';
+        $html .= '<td>' . $row['nome'] . '</td>';
+        $html .= '<td>' . $row['qtd'] . '</td>';
+        // Adiciona botões de excluir e editar
+        $html .= '<td>';
+        $html .= '<a href="excluir.php?id=' . $row['id'] . '">Excluir</a> | ';
+        $html .= '<a href="editar.php?id=' . $row['id'] . '">Editar</a>';
+        $html .= '</td>';
+        $html .= '</tr>';
+    }
+        
+        // Fecha a tag da tabela
+        $html .= '</table>';
+        
+        // Retorna a tabela HTML
+        return $html;
+    }
+    
 }
 
 // Exemplo de uso
--- includes/listagemProd.php
+++ includes/listagemProd.php
@@ -1,32 +1,9 @@
-<main>
-    <table class="table table-striped table-dark mt-2">
-    <thead>
-        <tr>
-        <th scope="col">#</th>
-        <th scope="col">Primeiro</th>
-        <th scope="col">Último</th>
-        <th scope="col">Nickname</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
-</main>
+<?php
+require_once './conexx/database.php';
+
+
+$database = new Database($conn);
+$tabelaHTML = $database->listarDados("produtos");
+
+echo $tabelaHTML;
+?>
