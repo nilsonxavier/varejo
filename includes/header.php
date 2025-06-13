d64141e184196a4993d140dad028b5172f7d815d
Nilson <nilsonxavier12@gmail.com>
2025-06-13 14:04:15 -0300

    Revert "Develop"

--- includes/header.php
+++ includes/header.php
@@ -11,9 +11,6 @@
 
   <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-
 <!-- Bootstrap JS Bundle -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
 
